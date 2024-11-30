1f0a52f30cf8274fa187c07e9b77b8aa9de684b2
tyson-lin <tysonlin@umich.edu>
2024-11-30 15:01:57 -0500

    x

--- src/StaticRouter.cpp
+++ src/StaticRouter.cpp
@@ -86,19 +86,27 @@ void StaticRouter::sendARP_Response(std::vector<uint8_t> packet, std::string ifa
     std::cout << "Request packet: " << std::endl;
     print_hdrs(packet.data(), packet.size());
 
+    RoutingInterface arrival_interface = routingTable->getRoutingInterface(iface);
+    mac_addr arrival_mac_addr = arrival_interface.mac;
+
     // First generate the ethernet header
     sr_ethernet_hdr_t* ehdr = (sr_ethernet_hdr_t*)packet.data();
     for (int i = 0; i < ETHER_ADDR_LEN; i++) { 
-        // source and destination get switched (opposite direction)
-        std::swap(ehdr->ether_dhost[i], ehdr->ether_shost[i]);
+        // set the destination to the source of the request packet
+        ehdr->ether_dhost[i] = ehdr->ether_shost[i];
+        // set the source to be the mac addr of the arrival interface
+        ehdr->ether_shost[i] = arrival_mac_addr[i];
     }
+    
     // packet type shouldn't change
 
     // Generate the arp header
     sr_arp_hdr_t* arp_hdr = (sr_arp_hdr_t*)(packet.data() + sizeof(sr_ethernet_hdr_t));
     for (int i = 0; i < ETHER_ADDR_LEN; i++) {
-        // sender and target hardware addresses get switched
-        std::swap(arp_hdr->ar_sha[i], arp_hdr->ar_tha[i]);
+        // target hardware address set to the sender hardware address
+        arp_hdr->tha[i] = arp_hdr->ar_sha[i];
+        // sender hardware address set to the mac addr of the arrival interface
+        arp_hdr->sha[i] = arrival_mac_addr[i];
     }
     // swap sender and target IP addresses    
     uint32_t temp_ip;
@@ -106,7 +114,7 @@ void StaticRouter::sendARP_Response(std::vector<uint8_t> packet, std::string ifa
     std::memcpy(&arp_hdr->ar_sip, &arp_hdr->ar_tip, sizeof(temp_ip)); // Replace sender IP with target IP
     std::memcpy(&arp_hdr->ar_tip, &temp_ip, sizeof(temp_ip)); // Replace target IP with original sender IP
     // formats and lengths shouldn't change
-    
+
     arp_hdr->ar_op = arp_op_reply;
 
     // Generate ARP response
