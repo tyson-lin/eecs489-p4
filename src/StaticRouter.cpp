311353f63315af9d3bf7b119119d6c5ecf966cd7
tyson-lin <tysonlin@umich.edu>
2024-11-30 21:37:22 -0500

    Update StaticRouter.cpp

--- src/StaticRouter.cpp
+++ src/StaticRouter.cpp
@@ -135,7 +135,7 @@ void StaticRouter::handleARP_Response(std::vector<uint8_t> packet, std::string i
     ip_addr arrival_ip = arrival_iface.ip;
 
     // Have I issued this response?
-    std::optional<mac_addr> entry_mac_addr = getEntry(arrival_ip);
+    std::optional<mac_addr> entry_mac_addr = arpCache->getEntry(arrival_ip);
     if (entry_mac_addr) {
         return; // Entry already exists in ARP cache
     }
